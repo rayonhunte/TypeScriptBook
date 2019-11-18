d8899655a6f9a82f299d3828079a287cb8ce21ed
rayonhunte <rayon.hunte@gmail.com>
2019-11-18 13:43:18 -0400

    added myLight function example

--- AdvanceTypes/StringLiteral.ts
+++ AdvanceTypes/StringLiteral.ts
@@ -18,6 +18,7 @@ class Room {
     }
 }
 
+// type usage a class instance
 // create a instance of the room class, enforce / define Room class type
 const livingRoom: Room = new Room();
 
@@ -31,5 +32,33 @@ console.log(livingRoom.lightSwitch);
 livingRoom.toggleSwitch("off");
 console.log(livingRoom.lightSwitch);
 
-// show an example  of error
+// show an example  of compilation error
+// comment out the below to run code without error
 livingRoom.toggleSwitch("up");
+
+
+// type usage with a function
+function myLight (turnSwitch: SwitchStatus): boolean  {
+    let light: boolean;
+
+    if (status === "on") {
+        light = true;
+    } else {
+        light = false;
+    }
+    return light;
+}
+
+// turn the switch on
+console.log(myLight("on"));
+
+// turn the switch off ;
+console.log(myLight("off"));
+
+// show an example of compilation error
+// comment out the below to run code without error
+myLight("Down")
+
+
+
+
